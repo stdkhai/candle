e3b93bad27758a5e9fbdbf5f7d243c61da7ea7d3
stdkhai <k.o.zhuk@student.khai,edu>
2023-03-14 17:45:27 +0200

    remove touch freezee

--- scripts/index.js
+++ scripts/index.js
@@ -26,15 +26,15 @@ var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewh
 function disableScroll() {
   window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
   window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
-  window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
-  window.addEventListener('keydown', preventDefaultForScrollKeys, false);
+/*   window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
+ */  window.addEventListener('keydown', preventDefaultForScrollKeys, false);
 }
 
 function enableScroll() {
   window.removeEventListener('DOMMouseScroll', preventDefault, false);
   window.removeEventListener(wheelEvent, preventDefault, wheelOpt);
-  window.removeEventListener('touchmove', preventDefault, wheelOpt);
-  window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
+/*   window.removeEventListener('touchmove', preventDefault, wheelOpt);
+ */  window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
 }
 
 //////////////////////////////////////////////////////
